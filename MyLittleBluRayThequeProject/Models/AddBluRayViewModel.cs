91e727027becaba1c40a002e91e66d7b68e399cc
chamarth <thomas.chamard@reseau.eseo.fr>
2021-12-07 17:24:29 +0100

    formulaire

---
+++ MyLittleBluRayThequeProject/Models/AddBluRayViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyLittleBluRayThequeProject.DTOs;
+
+namespace MyLittleBluRayThequeProject.Models
+{
+    public class AddBluRayViewModel
+    {
+        public int id { get; set; }
+        public string Titre { get; set; }
+
+        public Personne Scenariste { get; set; }
+
+        public Personne Realisateur { get; set; }
+
+        public List<Personne> Acteurs { get; set; }
+
+        public TimeSpan Duree { get; set; }
+
+        public DateTime DateSortie { get; set; }
+
+        public List<string> Langues { get; set; }
+
+        public List<string> SsTitres { get; set; }
+
+        public string Version { get; set; }
+
+    }
+}

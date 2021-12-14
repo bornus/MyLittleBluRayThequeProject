5fcb413735de6b5cd890f75ef8d9fdc7be5ea691
florent <florent.bellier@hotmail.fr>
2021-12-14 08:20:28 +0100

    feat(bdd) : connection PsSql

--- MyLittleBluRayThequeProject/Controllers/HomeController.cs
+++ MyLittleBluRayThequeProject/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace MyLittleBluRayThequeProject.Controllers
             model.BluRays = br.ConvertAll(InfoBluRayViewModel.ToModel);
             if (id != null)
             {
-                model.SelectedBluRay = br.FirstOrDefault(x => x.Id == id);
+                model.SelectedBluRay = brRepository.GetBluRay(id.Value);
             }
             return View(model);
         }
--- MyLittleBluRayThequeProject/Repositories/BluRayRepository.cs
+++ MyLittleBluRayThequeProject/Repositories/BluRayRepository.cs
@@ -1,4 +1,5 @@
 ﻿using MyLittleBluRayThequeProject.DTOs;
+using Npgsql;
 
 namespace MyLittleBluRayThequeProject.Repositories
 {
@@ -14,47 +15,85 @@ namespace MyLittleBluRayThequeProject.Repositories
 
         public List<BluRay> GetListeBluRay()
         {
-            return new List<BluRay>
-            { 
-                new BluRay
-                {
-                    Id = 0,
-                    Titre = "My Little film 1",
-                    DateSortie = DateTime.Now,
-                    Version = "Courte",
-                    Acteurs = new List<Personne>
+            NpgsqlConnection conn = null;
+            List<BluRay> result = new List<BluRay>();
+            try
+            {
+                // Connect to a PostgreSQL database
+                conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=psw;Database=postgres;");
+                conn.Open();
+
+                // Define a query returning a single row result set
+                NpgsqlCommand command = new NpgsqlCommand("SELECT \"Id\", \"Titre\", \"Duree\", \"Version\" FROM \"BluRayTheque\".\"BluRay\"", conn);
+
+                // Execute the query and obtain a result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                // Output rows
+                while (dr.Read())
+                    result.Add(new BluRay
                     {
-                        new Personne
-                        {
-                            Id = 0,
-                            Nom = "Per",
-                            Prenom = "Sonne",
-                            Nationalite = "Fr",
-                            DateNaissance = DateTime.Now,
-                            Professions = new List<string>{"Acteur"}
-                        }
-                    }
-                },
-                new BluRay
+                        Id = long.Parse(dr[0].ToString()),
+                        Titre = dr[1].ToString(),
+                        Duree = TimeSpan.FromSeconds(long.Parse(dr[2].ToString())),
+                        Version = dr[3].ToString()
+                    });
+
+            }
+            finally
+            {
+                if(conn != null)
                 {
-                    Id = 1,
-                    Titre = "My Little film 2",
-                    DateSortie = DateTime.Now,
-                    Version = "Longue",
-                    Acteurs = new List<Personne>
-                    {
-                        new Personne
-                        {
-                            Id = 0,
-                            Nom = "Per",
-                            Prenom = "Sonne",
-                            Nationalite = "Fr",
-                            DateNaissance = DateTime.Now,
-                            Professions = new List<string>{"Acteur"}
-                        }
-                    }
+                    conn.Close();
                 }
-            };
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Récupération d'un BR par son Id
+        /// </summary>
+        /// <param name="Id">l'Id du bluRay</param>
+        /// <returns></returns>
+        public BluRay GetBluRay(long Id)
+        {
+            NpgsqlConnection conn = null;
+            BluRay result = new BluRay();
+            try
+            {
+                List<BluRay> qryResult = new List<BluRay>();
+                // Connect to a PostgreSQL database
+                conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=psw;Database=postgres;");
+                conn.Open();
+
+                // Define a query returning a single row result set
+                NpgsqlCommand command = new NpgsqlCommand("SELECT \"Id\", \"Titre\", \"Duree\", \"Version\" FROM \"BluRayTheque\".\"BluRay\" where \"Id\" = @p", conn);
+                command.Parameters.AddWithValue("p", Id);
+
+                // Execute the query and obtain a result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                // Output rows
+                while (dr.Read())
+                    qryResult.Add(new BluRay
+                    {
+                        Id = long.Parse(dr[0].ToString()),
+                        Titre = dr[1].ToString(),
+                        Duree = TimeSpan.FromSeconds(long.Parse(dr[2].ToString())),
+                        Version = dr[3].ToString()
+                    });
+
+                result = qryResult.SingleOrDefault();
+
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return result;
         }
     }
 }

430b38b1073484f62da37923e89384264acdd606
chamarth <thomas.chamard@reseau.eseo.fr>
2022-01-11 10:51:37 +0100

    creation lien bluray acteurs/realisateur/scenariste

--- MyLittleBluRayThequeProject/Repositories/PersonneRepository.cs
+++ MyLittleBluRayThequeProject/Repositories/PersonneRepository.cs
@@ -44,6 +44,44 @@ namespace MyLittleBluRayThequeProject.Repositories
         }
 
 
+        public List<Personne> GetListActeurs()
+        {
+            NpgsqlConnection conn = null;
+            List<Personne> result = new List<Personne>();
+            try
+            {
+                // Connect to a PostgreSQL database
+                conn = new NpgsqlConnection("Server=127.0.0.1;User Id=postgres;Password=network;Database=postgres;");
+                conn.Open();
+
+                // Define a query returning a single row result set
+                NpgsqlCommand command = new NpgsqlCommand("SELECT \"p\".\"Id\", \"p\".\"Nom\", \"p\".\"Prenom\", \"p\".\"DateNaissance\", \"p\".\"Nationalite\" FROM \"BluRayTheque\".\"Personne\" AS p ", conn);
+
+                // Execute the query and obtain a result set
+                NpgsqlDataReader dr = command.ExecuteReader();
+
+                // Output rows
+                while (dr.Read())
+                    result.Add(new Personne
+                    {
+                        Id = long.Parse(dr["Id"].ToString()),
+                        Nom = dr["Nom"].ToString(),
+                        Prenom = dr["Prenom"].ToString(),
+                        DateNaissance = DateTime.Parse(dr["DateNaissance"].ToString()),
+                        Nationalite = dr["Nationalite"].ToString()
+                    });
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+            return result;
+        }
+
+
         public List<Personne> GetListRealisateurs()
         {
             NpgsqlConnection conn = null;
@@ -64,11 +102,11 @@ namespace MyLittleBluRayThequeProject.Repositories
                 while (dr.Read())
                     result.Add(new Personne
                     {
-                        Id = long.Parse(dr[0].ToString()),
-                        Nom = dr[1].ToString(),
-                        Prenom = dr[2].ToString(),
-                        DateNaissance = DateTime.Parse(dr[3].ToString()),
-                        Nationalite = dr[5].ToString()
+                        Id = long.Parse(dr["Id"].ToString()),
+                        Nom = dr["Nom"].ToString(),
+                        Prenom = dr["Prenom"].ToString(),
+                        DateNaissance = DateTime.Parse(dr["DateNaissance"].ToString()),
+                        Nationalite = dr["Nationalite"].ToString()
                     });
             }
             finally
@@ -101,11 +139,11 @@ namespace MyLittleBluRayThequeProject.Repositories
                 while (dr.Read())
                     result.Add(new Personne
                     {
-                        Id = long.Parse(dr[0].ToString()),
-                        Nom = dr[1].ToString(),
-                        Prenom = dr[2].ToString(),
-                        DateNaissance = DateTime.Parse(dr[3].ToString()),
-                        Nationalite = dr[5].ToString()
+                        Id = long.Parse(dr["Id"].ToString()),
+                        Nom = dr["Nom"].ToString(),
+                        Prenom = dr["Prenom"].ToString(),
+                        DateNaissance = DateTime.Parse(dr["DateNaissance"].ToString()),
+                        Nationalite = dr["Nationalite"].ToString()
                     });
             }
             finally
